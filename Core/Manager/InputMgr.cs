98635f1c94ed06183de48405d78feacddbb2eafe
HHG\rdyho <rdyhong@gmail.com>
2025-08-13 12:45:23 +0900

    * 인풋 매니저 수정중

--- Core/Manager/InputMgr.cs
+++ Core/Manager/InputMgr.cs
@@ -7,12 +7,12 @@ public class InputMgr : MonoBehaviour
     public static Vector3 StickDir;
     public static float StickWeight;
 
-    public static bool IsFirePress()
+    public static bool IsSpawn()
     {
         return Input.GetMouseButton(0);
     }
 
-    public static bool IsPunchPress()
+    public static bool IsAttack()
     {
         return Input.GetMouseButton(1);
     }

5238457e7a1b7a8b0fddf95ea58f724d51081910
rdyhong <rdyhong@gmail.com>
2025-09-14 04:43:39 +0900

    * 풀링 매니저 개선

--- Core/Manager/ObjectPoolMgr.cs
+++ Core/Manager/ObjectPoolMgr.cs
@@ -8,7 +8,6 @@ public interface IPoolingObject
     void OnSpawn();
     void OnRecycle();
 }
-
 public class ObjectPoolMgr : Singleton<ObjectPoolMgr>, IPunPrefabPool
 {
     private Transform _parent = null;
@@ -17,6 +16,9 @@ public class ObjectPoolMgr : Singleton<ObjectPoolMgr>, IPunPrefabPool
 
     // Photon 전용 풀 (네트워크 오브젝트)
     private readonly Dictionary<string, Queue<GameObject>> _photonPool = new Dictionary<string, Queue<GameObject>>();
+
+    // Photon 풀에서 사용 중인 오브젝트 추적
+    private readonly Dictionary<string, List<GameObject>> _photonPoolUsing = new Dictionary<string, List<GameObject>>();
 
     protected override void Awake()
     {
@@ -28,6 +30,160 @@ public class ObjectPoolMgr : Singleton<ObjectPoolMgr>, IPunPrefabPool
         PhotonNetwork.PrefabPool = this;
     }
 
+    #region ===== Photon Instantiate 풀링 =====
+    public GameObject Instantiate(string prefabId, Vector3 position, Quaternion rotation)
+    {
+        GameObject obj = null;
+
+        if (_photonPool.TryGetValue(prefabId, out var queue) && queue.Count > 0)
+        {
+            obj = queue.Dequeue();
+            obj.transform.SetPositionAndRotation(position, rotation);
+            obj.SetActive(true);
+            Debug.Log("Spawnd in Pool");
+        }
+        else
+        {
+            GameObject prefab = Resources.Load<GameObject>(prefabId);
+            obj = Object.Instantiate(prefab, position, rotation);
+            Debug.Log("Spawn new");
+        }
+
+        // Photon 사용 중인 오브젝트 추적에 추가
+        if (!_photonPoolUsing.ContainsKey(prefabId))
+        {
+            _photonPoolUsing[prefabId] = new List<GameObject>();
+        }
+        _photonPoolUsing[prefabId].Add(obj);
+
+        var poolingObj = obj.GetComponent<IPoolingObject>();
+        poolingObj?.OnSpawn();
+
+        return obj;
+    }
+
+    public void Destroy(GameObject gameObject)
+    {
+        if (gameObject == null) return;
+
+        string prefabId = gameObject.name.Replace("(Clone)", "").Trim();
+
+        // 사용 중인 목록에서 제거
+        if (_photonPoolUsing.ContainsKey(prefabId) && _photonPoolUsing[prefabId].Contains(gameObject))
+        {
+            _photonPoolUsing[prefabId].Remove(gameObject);
+        }
+
+        if (!_photonPool.ContainsKey(prefabId))
+            _photonPool[prefabId] = new Queue<GameObject>();
+
+        gameObject.SetActive(false);
+        gameObject.transform.SetParent(_parent);
+        gameObject.transform.position = new Vector3(0, 2000, 0); // 위치 초기화
+
+        var poolingObj = gameObject.GetComponent<IPoolingObject>();
+        poolingObj?.OnRecycle();
+
+        _photonPool[prefabId].Enqueue(gameObject);
+    }
+
+    public void PreloadPhoton(string prefabId, int count)
+    {
+        if (!_photonPool.ContainsKey(prefabId))
+            _photonPool[prefabId] = new Queue<GameObject>();
+
+        GameObject prefab = Resources.Load<GameObject>(prefabId);
+
+        for (int i = 0; i < count; i++)
+        {
+            GameObject obj = Object.Instantiate(prefab);
+            obj.SetActive(false);
+            obj.transform.SetParent(_parent);
+            _photonPool[prefabId].Enqueue(obj);
+        }
+    }
+    #endregion
+
+    #region ===== 전체 회수 기능 =====
+    public void RecycleAll()
+    {
+        // 기존 로컬 풀 오브젝트들 회수
+        var keysToProcess = new List<string>(_objPoolUsing.Keys);
+        foreach (string key in keysToProcess)
+        {
+            var objectsToRecycle = new List<GameObject>(_objPoolUsing[key]);
+            foreach (GameObject obj in objectsToRecycle)
+            {
+                Recycle(obj);
+            }
+        }
+
+        // Photon 풀 오브젝트들 회수
+        var photonKeysToProcess = new List<string>(_photonPoolUsing.Keys);
+        foreach (string key in photonKeysToProcess)
+        {
+            var objectsToRecycle = new List<GameObject>(_photonPoolUsing[key]);
+            foreach (GameObject obj in objectsToRecycle)
+            {
+                Destroy(obj); // Photon 풀의 Destroy 메서드 사용
+            }
+        }
+    }
+
+    // 로컬 풀만 회수
+    public void RecycleAllLocal()
+    {
+        var keysToProcess = new List<string>(_objPoolUsing.Keys);
+        foreach (string key in keysToProcess)
+        {
+            var objectsToRecycle = new List<GameObject>(_objPoolUsing[key]);
+            foreach (GameObject obj in objectsToRecycle)
+            {
+                Recycle(obj);
+            }
+        }
+    }
+
+    // Photon 풀만 회수
+    public void RecycleAllPhoton()
+    {
+        var photonKeysToProcess = new List<string>(_photonPoolUsing.Keys);
+        foreach (string key in photonKeysToProcess)
+        {
+            var objectsToRecycle = new List<GameObject>(_photonPoolUsing[key]);
+            foreach (GameObject obj in objectsToRecycle)
+            {
+                Destroy(obj);
+            }
+        }
+    }
+
+    // 풀 상태 확인용 디버그 메서드
+    public void LogPoolStatus()
+    {
+        Debug.Log("=== Local Pool Status ===");
+        foreach (var pool in _objPoolUsing)
+        {
+            Debug.Log($"Local Using - {pool.Key}: {pool.Value.Count} objects");
+        }
+        foreach (var pool in _objPool)
+        {
+            Debug.Log($"Local Available - {pool.Key}: {pool.Value.Count} objects");
+        }
+
+        Debug.Log("=== Photon Pool Status ===");
+        foreach (var pool in _photonPoolUsing)
+        {
+            Debug.Log($"Photon Using - {pool.Key}: {pool.Value.Count} objects");
+        }
+        foreach (var pool in _photonPool)
+        {
+            Debug.Log($"Photon Available - {pool.Key}: {pool.Value.Count} objects");
+        }
+    }
+    #endregion
+
+    // 기존 코드들...
     #region ===== 기존 로컬 풀 기능 =====
     public T Spawn<T>(string path) where T : MonoBehaviour
     {
@@ -58,11 +214,10 @@ public class ObjectPoolMgr : Singleton<ObjectPoolMgr>, IPunPrefabPool
         }
         _objPoolUsing[path].Add(go);
         go.name = path;
-        if(go.TryGetComponent(out IPoolingObject poolObj))
+        if (go.TryGetComponent(out IPoolingObject poolObj))
         {
             poolObj.OnSpawn();
         }
-        //go.GetComponent<IPoolingObject>().OnSpawn();
         go.SetActive(true);
         return go.GetComponent<T>();
     }
@@ -99,7 +254,7 @@ public class ObjectPoolMgr : Singleton<ObjectPoolMgr>, IPunPrefabPool
             _objPool[path] = new List<GameObject>();
         }
 
-        for(int i = 0; i < count; i++)
+        for (int i = 0; i < count; i++)
         {
             go = Instantiate(Resources.Load(path) as GameObject);
             go.transform.SetParent(_parent);
@@ -125,78 +280,6 @@ public class ObjectPoolMgr : Singleton<ObjectPoolMgr>, IPunPrefabPool
             return result;
         }
         return null;
-    }
-
-    public void RecycleAll()
-    {
-        var keysToProcess = new List<string>(_objPoolUsing.Keys);
-        foreach (string key in keysToProcess)
-        {
-            var objectsToRecycle = new List<GameObject>(_objPoolUsing[key]);
-            foreach (GameObject obj in objectsToRecycle)
-            {
-                Recycle(obj);
-            }
-        }
-    }
-    #endregion
-
-    #region ===== Photon Instantiate 풀링 =====
-    public GameObject Instantiate(string prefabId, Vector3 position, Quaternion rotation)
-    {
-        GameObject obj = null;
-
-        if (_photonPool.TryGetValue(prefabId, out var queue) && queue.Count > 0)
-        {
-            obj = queue.Dequeue();
-            obj.transform.SetPositionAndRotation(position, rotation);
-            obj.SetActive(true);
-            Debug.Log("Spawnd in Pool");
-        }
-        else
-        {
-            GameObject prefab = Resources.Load<GameObject>(prefabId);
-            obj = Object.Instantiate(prefab, position, rotation);
-            Debug.Log("Spawn new");
-        }
-
-        var poolingObj = obj.GetComponent<IPoolingObject>();
-        poolingObj?.OnSpawn();
-
-        return obj;
-    }
-
-    public void Destroy(GameObject gameObject)
-    {
-        if (gameObject == null) return;
-
-        string prefabId = gameObject.name.Replace("(Clone)", "").Trim();
-        if (!_photonPool.ContainsKey(prefabId))
-            _photonPool[prefabId] = new Queue<GameObject>();
-
-        gameObject.SetActive(false);
-        gameObject.transform.SetParent(_parent);
-
-        var poolingObj = gameObject.GetComponent<IPoolingObject>();
-        poolingObj?.OnRecycle();
-
-        _photonPool[prefabId].Enqueue(gameObject);
-    }
-
-    public void PreloadPhoton(string prefabId, int count)
-    {
-        if (!_photonPool.ContainsKey(prefabId))
-            _photonPool[prefabId] = new Queue<GameObject>();
-
-        GameObject prefab = Resources.Load<GameObject>(prefabId);
-
-        for (int i = 0; i < count; i++)
-        {
-            GameObject obj = Object.Instantiate(prefab);
-            obj.SetActive(false);
-            obj.transform.SetParent(_parent);
-            _photonPool[prefabId].Enqueue(obj);
-        }
     }
     #endregion
 }
